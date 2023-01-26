8b139ca8c2dd07622c2f612231a8dfe73eee0741
salom <salom@Scooby.dhcp.wustl.edu>
2023-01-26 11:45:09 -0600

    Share

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -6,7 +6,8 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+		System.out.println("What is your name?");
+		String name=in.nextLine();
 		System.out.println("How are you feeling today?");
 		String mood = in.nextLine();
 		
--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -10,9 +10,12 @@ public class HiOne {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String name = in.nextLine();
-		//
+		System.out.println("What is your name?");
+		String name=in.nextLine();
+		System.out.println("How are you?");
+		String mood = in.nextLine();
+		
+		System.out.println(" Hi "+ name +" is "+ mood +  ".");
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
 		// if the value of name is "Pat"
